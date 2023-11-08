f0d65228af863cf4fe1fc86b5a4b588bdb1664ba
Eugenia <st_shelkunova@list.ru>
2023-11-08 21:48:06 +0300

    src

---
+++ src/test/java/ru/netology/services/RestServiceTest.java
@@ -0,0 +1,36 @@
+package ru.netology.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RestServiceTest {
+
+    @Test
+
+    void shouldCalcForRestAndWork1() {
+      RestService service = new RestService();
+
+      int income = 10_000;
+      int expenses = 3_000;
+      int threshold = 20_000;
+      int expected = 3;
+
+      int actual = (int)service.calculate(income, expenses, threshold);
+
+        Assertions.assertEquals (expected, actual);
+    }
+    @Test
+
+    void shouldCalcForRestAndWork2() {
+        RestService service = new RestService();
+
+        int income = 100_000;
+        int expenses = 60_000;
+        int threshold = 150_000;
+        int expected = 2;
+
+        long actual = service.calculate(income, expenses, threshold);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
